5675105997a4fc8531718a7d38a14daf350cd11c
ganeev-ruslan <145503389+ganeev-ruslan@users.noreply.github.com>
2024-10-18 18:38:36 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,51 +1,28 @@
-$(document).ready(function () {
-  $(".hamburger").on("click", function () {
-    $(".mobile-menu").addClass("is-active");
-  });
 
-  $(".mobile-menu-close").on("click", function () {
-    $(".mobile-menu").removeClass("is-active");
-  });
-});
-
-
-
-
-
-$(document).ready(function() {
-    // Обновление данных о подписчиках и аватарах
-    function updateSubscriberData() {
-        $.ajax({
-            url: 'server.php',
-            type: 'GET',
-            dataType: 'json',
-            success: function(data) {
-                // Обновление счетчика
-                $('.counter').text(data.count);
-                // Обновление аватаров
-                $('.avatar').each(function(index) {
-                    if (data.avatars[index]) {
-                        $(this).css('background-image', 'url(' + data.avatars[index] + ')');
-                        $(this).addClass('active');
-                    } else {
-                        $(this).css('background-image', 'none');
-                        $(this).removeClass('active');
-                    }
-                });
-            },
-            error: function(error) {
-                console.error('Ошибка получения данных:', error);
-            }
-        });
-    }// Вызов обновления при загрузке страницы
-updateSubscriberData();
-
-// Обработка кликов по аватарам
-$('.avatar').click(function() {
-    // Переход на сайт приложения ВКонтакте
-    window.location.href = "https://vk.com/club41304908"; // Замените ссылку на ваше приложение
-});
-
-// Периодическое обновление данных
-setInterval(updateSubscriberData, 30000); // Обновление каждые 30 секунд
+let swiper = new Swiper(".gallery-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  pagination: {
+    el: ".gallery-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".gallery-btn_next",
+    prevEl: ".gallery-btn_prev"
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    
+    },
+    1274: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+  },
 });

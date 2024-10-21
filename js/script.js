5bab8ce75247c1032ba5881f8ef7b896fdcee8b1
ganeev-ruslan <145503389+ganeev-ruslan@users.noreply.github.com>
2024-10-21 15:42:16 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ let swiper = new Swiper(".gallery-swiper", {
   },
   navigation: {
     nextEl: ".gallery-btn_next",
-    prevEl: ".gallery-btn_prev"
+    prevEl: ".gallery-btn_prev",
   },
   breakpoints: {
     640: {
@@ -18,11 +18,38 @@ let swiper = new Swiper(".gallery-swiper", {
     768: {
       slidesPerView: 2,
       spaceBetween: 20,
-    
     },
     1274: {
       slidesPerView: 3,
       spaceBetween: 20,
     },
   },
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const monthlyButton = document.getElementById("monthly");
+  const yearlyButton = document.getElementById("yearly");
+
+  monthlyButton.addEventListener("click", () => {
+    changeButtonColor(monthlyButton);
+    changeButtonColor(yearlyButton);
+  });
+
+  yearlyButton.addEventListener("click", () => {
+    changeButtonColor(yearlyButton);
+    changeButtonColor(monthlyButton);
+  });
+
+
+  function changeButtonColor(button) {
+    if (button.classList.contains('monthly_color')) {
+      button.classList.remove('monthly_color');
+      button.classList.add('yearly_color');
+    } else {
+      button.classList.remove('yearly_color');
+      button.classList.add('monthly_color');
+    }
+
+    
+  }
 });

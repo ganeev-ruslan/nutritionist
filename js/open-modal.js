bf0c02091a106ae1650eccb27faef198828e0473
ganeev-ruslan <145503389+ganeev-ruslan@users.noreply.github.com>
2025-01-11 14:45:40 +0300

    Create open-modal.js

---
+++ js/open-modal.js
@@ -0,0 +1,32 @@
+document
+  .getElementById("open-modal-btn")
+  .addEventListener("click", function () {
+    document.getElementById("my-modal").classList.add("open");
+  });
+
+document
+  .getElementById("close-my-modal-btn")
+  .addEventListener("click", function () {
+    document.getElementById("my-modal").classList.remove("open");
+  });
+
+
+// закрытие модального окна по esc
+
+window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+        document.getElementById("my-modal").classList.remove("open")
+    }
+})
+
+
+// закрытие модального окна при клике вне его 
+
+document.querySelector("#my-modal .modal__box").addEventListener('click', event => {
+    event._isClickWithInModal = true;
+});
+
+document.getElementById("my-modal").addEventListener("click", event =>{
+    if (event._isClickWithInModal) return;
+    event.currentTarget.classList.remove('open')
+});

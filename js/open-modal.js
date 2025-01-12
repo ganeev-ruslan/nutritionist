f5b2094724047eaa9273d6f544787868a2d4f0aa
ganeev-ruslan <145503389+ganeev-ruslan@users.noreply.github.com>
2025-01-12 14:09:33 +0300

    Update open-modal.js

--- js/open-modal.js
+++ js/open-modal.js
@@ -1,14 +1,14 @@
 document
-  .getElementById("open-modal-btn")
-  .addEventListener("click", function () {
-    document.getElementById("my-modal").classList.add("open");
-  });
+    .getElementById("open-modal-btn")
+    .addEventListener("click", function () {
+        document.getElementById("my-modal").classList.add("open");
+    });
 
 document
-  .getElementById("close-my-modal-btn")
-  .addEventListener("click", function () {
-    document.getElementById("my-modal").classList.remove("open");
-  });
+    .getElementById("close-my-modal-btn")
+    .addEventListener("click", function () {
+        document.getElementById("my-modal").classList.remove("open");
+    });
 
 
 // закрытие модального окна по esc
@@ -26,7 +26,50 @@ document.querySelector("#my-modal .modal__box").addEventListener('click', event 
     event._isClickWithInModal = true;
 });
 
-document.getElementById("my-modal").addEventListener("click", event =>{
+document.getElementById("my-modal").addEventListener("click", event => {
     if (event._isClickWithInModal) return;
     event.currentTarget.classList.remove('open')
 });
+
+
+
+document
+    .getElementById("open-modal-btn-1")
+    .addEventListener("click", function () {
+        document.getElementById("my-modal-1").classList.add("open");
+    });
+
+document
+    .getElementById("close-my-modal-btn-1")
+    .addEventListener("click", function () {
+        document.getElementById("my-modal-1").classList.remove("open");
+    });
+
+
+// закрытие модального окна по esc
+
+window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+        document.getElementById("my-modal-1").classList.remove("open")
+    }
+})
+
+
+// закрытие модального окна при клике вне его 
+
+document.querySelector("#my-modal .modal__box").addEventListener('click', event => {
+    event._isClickWithInModal = true;
+});
+
+document.getElementById("my-modal").addEventListener("click", event => {
+    if (event._isClickWithInModal) return;
+    event.currentTarget.classList.remove('open')
+});
+
+
+
+
+
+
+
+
